67b03ffd9d403420b064dfd28ac00f5dd8381d18
treethought <cpsweene@gmail.com>
2020-05-25 10:44:47 -0700

    log to file instead of log view

--- pkg/ui/utils.go
+++ pkg/ui/utils.go
@@ -1,7 +1,6 @@
 package ui
 
 import (
-	// "strings"
 	"fmt"
 
 	"github.com/jroimartin/gocui"
@@ -44,14 +43,7 @@ func cursorUp(g *gocui.Gui, v *gocui.View) error {
 
 // writeLog writes the message to the log UI view
 func (ui *UI) writeLog(a ...interface{}) error {
-
-	v, err := ui.g.View("log_view")
-	if err != nil {
-		return err
-	}
-	fmt.Fprintln(v, a...)
-	ui.updateUI()
-
+	ui.log(a...)
 	return nil
 
 }
